56cf912d8b4bffab858f1ade313f001de770f3e1
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-02-01 22:09:59 +0200

    initial footer design

--- bus-app/src/app/app.ts
+++ bus-app/src/app/app.ts
@@ -4,12 +4,12 @@ import { AddStop } from "./features/stops/add-stop/add-stop";
 import { AddRoute } from "./features/routes/add-route/add-route";
 import { AddTransportOperator } from './features/add-transport-operator/add-transport-operator';
 import { RouteData } from './features/routes/route-data/route-data';
-import { Header } from './shared/components';
+import { Footer, Header } from './shared/components';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AddStop, AddRoute, AddTransportOperator, RouteData, Header],
+  imports: [RouterOutlet, AddStop, AddRoute, AddTransportOperator, RouteData, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
