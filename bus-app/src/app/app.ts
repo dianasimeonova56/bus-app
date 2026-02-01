a4afe2e347a6cf645b70b736ad40e2c8e048ca99
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-02-01 21:49:25 +0200

    initial navbar design

--- bus-app/src/app/app.ts
+++ bus-app/src/app/app.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MapComponent } from "./shared/components/map/map";
 import { AddStop } from "./features/stops/add-stop/add-stop";
 import { AddRoute } from "./features/routes/add-route/add-route";
 import { AddTransportOperator } from './features/add-transport-operator/add-transport-operator';
-import { LoadDepartures } from './features/routes/load-departures/load-departures';
 import { RouteData } from './features/routes/route-data/route-data';
+import { Header } from './shared/components';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AddStop, AddRoute, AddTransportOperator, RouteData],
+  imports: [RouterOutlet, AddStop, AddRoute, AddTransportOperator, RouteData, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
