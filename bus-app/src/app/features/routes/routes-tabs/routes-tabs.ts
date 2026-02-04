7cd14645fa3fdbe188b41cb13b2559cce20ab517
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-02-04 23:09:37 +0200

    routes tabs for swithcing between stations -arrivals/departures

--- bus-app/src/app/features/routes/routes-tabs/routes-tabs.ts
+++ bus-app/src/app/features/routes/routes-tabs/routes-tabs.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {Tab, Tabs, TabList, TabPanel, TabContent} from '@angular/aria/tabs';
+import { RouteData } from "../route-data/route-data";
 
 @Component({
   selector: 'app-routes-tabs',
-  imports: [],
+  imports: [RouteData, TabList, Tab, Tabs, TabPanel, TabContent],
   templateUrl: './routes-tabs.html',
   styleUrl: './routes-tabs.css',
 })
