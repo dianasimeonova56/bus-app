8e47701239786cfcc84ac8c58ccca6b9f8ceceed
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-02-04 23:08:03 +0200

    modify FE models for selected stop and stop  to have address

--- bus-app/src/app/models/selectedStop.model.ts
+++ bus-app/src/app/models/selectedStop.model.ts
@@ -1,5 +1,5 @@
 export interface SelectedStop {
   lat: number;
   lng: number;
-  name: string;
+  address: string;
 }
--- bus-app/src/app/models/stop.model.ts
+++ bus-app/src/app/models/stop.model.ts
@@ -1,6 +1,7 @@
 export interface Stop {
     _id?: string;
     name: string;
+    address: string,
     location: {
         type: string;
         coordinates: [number, number];
