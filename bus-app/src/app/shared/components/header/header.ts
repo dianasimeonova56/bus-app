9a860de8dca3dc07f789ebadfe30cea5dea8e374
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-03-01 17:18:47 +0200

    frontend: header use guards to show link items

--- bus-app/src/app/shared/components/header/header.ts
+++ bus-app/src/app/shared/components/header/header.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  protected authService = inject(AuthService)
+  private router = inject(Router);
 
+  readonly isLoggedIn = this.authService.isLoggedIn;
+  readonly currentUser = this.authService.currentUser;
+
+  protected role = this.currentUser()?.user_role;
+  openMenu: boolean = false;
+
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/home'])
+      }, error: (err) => {
+        console.log("Logout error", err);
+      }
+    });
+  }
 }
