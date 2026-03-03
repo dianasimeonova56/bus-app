ce25bcdc90414e921dde8c16f3005f1c70b6c0bb
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-03-03 23:43:39 +0200

    BE: add tripsService and move over logic from controller, add endpoints for stations(might not be needed)

--- backend/controllers/tripsController.js
+++ backend/controllers/tripsController.js
@@ -1,15 +1,24 @@
 import express from 'express';
-import Trip from '../models/Trip.js';
+import tripsService from '../services/tripsService.js';
+import routesService from '../services/routesService.js';
 
 const tripsController = express.Router();
 
+tripsController.get('/', async (req, res) => {
+    try {
+        const trips = tripsService.getAll();
+        res.json(trips);
+    } catch (err) {
+        res.status(404).json({ message: 'No trips found' });
+    }
+});
+
 tripsController.get('/:id', async (req, res) => {
     try {
-        const trip = await Trip.findById(req.params.id)
-            .populate({
-                path: 'route',
-                populate: { path: 'startStop.stopId endStop.stopId stops.stopId transportOperator' }
-            });
+        const tripId = req.params.id;
+        
+        const trip = await tripsService.getOne(tripId);
+
         res.json(trip);
     } catch (err) {
         res.status(404).json({ message: 'Trip not found' });
@@ -18,12 +27,38 @@ tripsController.get('/:id', async (req, res) => {
 
 tripsController.patch('/:id/cancel', async (req, res) => {
     try {
-        const updatedTrip = await Trip.findByIdAndUpdate(
-            req.params.id, 
-            { status: 'cancelled' }, 
-            { new: true }
-        );
+        const tripId = req.params.tripId;
+        const updatedTrip = await tripsService.cancel(tripId);
         res.json(updatedTrip);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+const getStationByPosition = async (stationParam) => {
+    const mainStations = await Stop.find({ isMainStation: true });
+    return mainStations.find(s => 
+        (stationParam === 'south' && s.south) || (stationParam === 'west' && s.west)
+    );
+};
+
+tripsController.get('/departures/:station', async (req, res) => {
+    try {
+        const stationObj = await getStationByPosition(req.params.station);
+        if (!stationObj) return res.status(404).json({ message: 'Station not found' });
+        const routes = await routesService.getStationDepartures(stationObj._id);
+        res.status(200).json(routes);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+tripsController.get('/arrivals/:station', async (req, res) => {
+    try {
+        const stationObj = await getStationByPosition(req.params.station);
+        if (!stationObj) return res.status(404).json({ message: 'Station not found' });
+        const routes = await routesService.getStationArrivals(stationObj._id);
+        res.status(200).json(routes);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
---
+++ backend/services/tripsService.js
@@ -0,0 +1,24 @@
+import Trip from '../models/Trip.js';
+import mongoose from 'mongoose';
+export default {
+    getAll() {
+        return Trip.find();
+    },
+    async getOne(tripId){
+        const updatedTrip = await Trip.findById(tripId)
+            .populate({path: 'route',
+                populate: { path: 'startStop.stopId endStop.stopId stops.stopId' }});
+        console.log(updatedTrip);
+        
+        return updatedTrip;
+    },
+    async cancel(tripId){
+        const updatedTrip = await Trip.findByIdAndUpdate(
+            tripId,
+            { status: 'cancelled' }, 
+            { new: true }
+        );
+        return updatedTrip;
+    },
+
+}
