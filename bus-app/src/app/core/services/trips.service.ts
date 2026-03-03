e2ae7062fddaec44a282552e9f523e1af22bee53
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-03-03 23:47:55 +0200

    FE: fixes on trips service

--- bus-app/src/app/core/services/trips.service.ts
+++ bus-app/src/app/core/services/trips.service.ts
@@ -1,18 +1,18 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { Observable } from "rxjs";
 import { Trip } from "../../models/trip.model";
 
 @Injectable({ providedIn: 'root' })
 export class TripsService {
-    private http = inject(HttpClient);
+    private httpClient = inject(HttpClient);
     private apiUrl = 'http://localhost:3000/trips';
 
     getTripById(id: string): Observable<Trip> {
-        return this.http.get<Trip>(`${this.apiUrl}/${id}`);
+        return this.httpClient.get<Trip>(`${this.apiUrl}/${id}`);
     }
 
     cancelTrip(id: string): Observable<Trip> {
-        return this.http.patch<Trip>(`${this.apiUrl}/${id}/cancel`, {});
+        return this.httpClient.patch<Trip>(`${this.apiUrl}/${id}/cancel`, {});
     }
 }
