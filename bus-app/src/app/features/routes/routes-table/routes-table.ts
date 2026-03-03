6865b0b33176d497ca17dfe482ec6d4bf9817797
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-03-03 23:47:26 +0200

    FE: updates on routes table

--- bus-app/src/app/features/routes/routes-table/routes-table.ts
+++ bus-app/src/app/features/routes/routes-table/routes-table.ts
@@ -1,19 +1,18 @@
 import { Component, inject, Input, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
-import { RoutesService } from '../../../core/services';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { AsyncPipe, CommonModule } from '@angular/common'
 import { HighlightStopDirective } from '../../../shared/directive';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-routes-table',
   standalone: true,
-  imports: [AsyncPipe, HighlightStopDirective, CommonModule],
+  imports: [AsyncPipe, HighlightStopDirective, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './routes-table.html',
   styleUrl: './routes-table.css',
 })
 export class RoutesTable implements OnChanges {
-  private routesService = inject(RoutesService);
-  private cdr = inject(ChangeDetectorRef); // Добавено за сигурност
+  private cdr = inject(ChangeDetectorRef);
 
   @Input() data: any[] | null = null; 
   @Input() searchedStopId: string | null = null;
@@ -23,15 +22,12 @@ export class RoutesTable implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['data']) {
-      // Актуализираме потока от данни
       this.dataSubject.next(this.data || []);
-      // Казваме на Angular да провери за промени веднага
       this.cdr.detectChanges();
     }
   }
 
   isTrip(item: any): boolean {
-    // Проверка дали обектът е Trip (има вложен route)
     return !!(item && item.route);
   }
 
@@ -43,6 +39,6 @@ export class RoutesTable implements OnChanges {
 
   toggle(id: string | undefined) {
     this.expandedRouteId = this.expandedRouteId === id ? undefined : id;
-    this.cdr.detectChanges(); // За да се отвори/затвори панелът веднага
+    this.cdr.detectChanges();
   }
 }
