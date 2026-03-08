32c33ff99e35e076dc7ee4d1224793be8dc8666a
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-03-08 23:22:20 +0200

    FE: booking behavor when not working with passed tripid - render 2 departure station and their corresponding stops for different routes - render times/dates

--- bus-app/src/app/features/booking/booking.ts
+++ bus-app/src/app/features/booking/booking.ts
@@ -5,7 +5,7 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { startWith } from 'rxjs';
 import { TripsService } from '../../core/services/trips.service';
 import { Stop, Trip, User } from '../../models';
-import { AuthService } from '../../core/services';
+import { AuthService, StopsService } from '../../core/services';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from '../../shared/components/map/map';
 import { Login } from '../auth/login/login';
@@ -22,19 +22,27 @@ export class Booking {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private tripsService = inject(TripsService);
+  private stopsService = inject(StopsService);
   private authService = inject(AuthService);
 
   bookingForm: FormGroup = this.formBuilder.group({
+    mainStation: [''],
     trip: ['', [Validators.required]],
     departure: ['', [Validators.required]],
     destination: ['', [Validators.required]],
     ticket_num: [1, [Validators.required, Validators.min(1)]],
-    ticketType: ['oneWayTicket']
+    ticketType: ['oneWayTicket'],
+    departureTime: ['', [Validators.required]],
+    date: [new Date().toISOString().split('T')[0], [Validators.required]],
   });
 
   currentTrip = signal<Trip | null>(null);
   basePrice = signal<number>(0);
   currentUser: User | null = this.authService.currentUser();
+  isManualSelection = signal<boolean>(false);
+  allAvailableStops = toSignal(this.stopsService.stops$, { initialValue: [] });
+  availableTrips = signal<Trip[]>([]);
+  minDate: string = new Date().toISOString().split('T')[0];
 
   formValues = toSignal(
     this.bookingForm.valueChanges.pipe(startWith(this.bookingForm.value))
@@ -63,12 +71,53 @@ export class Booking {
           this.bookingForm.setValidators(this.routeOrderValidator());
 
           const initialPrice = ticketInfo ? ticketInfo.price : trip.route.oneWayTicketPrice;
-          
+
           this.basePrice.set(initialPrice);
           this.totalPrice()
         }
       });
+    } else {
+      this.isManualSelection.set(true);
+      this.stopsService.getStops().subscribe();
     }
+
+    this.bookingForm.get('mainStation')?.valueChanges.subscribe(stationId => {
+      if (stationId) {
+        const selectedHub = this.allAvailableStops().find(s => s._id === stationId);
+
+        if (selectedHub) {
+          this.bookingForm.patchValue({ departure: selectedHub.name, destination: '' });
+
+          const type = selectedHub.south ? 'south' : 'west';
+
+          this.tripsService.getDepartures(type).subscribe(res => {
+            this.availableTrips.set(res.docs);
+          });
+        }
+      }
+    });
+
+    this.bookingForm.get('destination')?.valueChanges.subscribe(() => {
+      this.bookingForm.patchValue({ departureTime: '', trip: '' }, { emitEvent: false });
+      this.currentTrip.set(null);
+    });
+
+    this.bookingForm.get('departureTime')?.valueChanges.subscribe(tripId => {
+      const foundTrip = this.availableTimesForRoute().find(t => t._id === tripId);
+
+      if (foundTrip) {
+        this.currentTrip.set(foundTrip);
+        console.log(this.currentTrip, this.currentPath());
+        
+        this.bookingForm.patchValue({ trip: foundTrip._id }, { emitEvent: false });
+
+        const destName = this.bookingForm.get('destination')?.value;
+        const stopInfo = foundTrip.route.stops.find(s => s.stopId.name === destName);
+        const price = stopInfo ? stopInfo.price : foundTrip.route.oneWayTicketPrice;
+
+        this.basePrice.set(price);
+      }
+    });
   }
 
   allStopsInOrder = computed(() => {
@@ -115,7 +164,6 @@ export class Booking {
   });
 
   totalPrice = computed(() => {
-    
     const trip = this.currentTrip();
     const values = this.formValues();
     if (!values) return 0;
@@ -130,6 +178,56 @@ export class Booking {
 
     const total = pricePerTicket * count;
     return Math.round(total * 100) / 100;
+  });
+
+  destinationOptions = computed(() => {
+    const trips = this.availableTrips();
+    if (trips.length === 0) return [];
+
+    const allPossibleDestinations = trips.flatMap(trip => {
+      return [
+        trip.route.endStop.stopId,
+        ...trip.route.stops.map(s => s.stopId)
+      ];
+    });
+
+    return allPossibleDestinations.filter((stop, index, self) =>
+      index === self.findIndex((s) => s.name === stop.name)
+    );
+  });
+
+  mainHubs = computed(() => {
+    const stops = this.allAvailableStops();
+    if (this.allAvailableStops().length === 0) {
+      this.stopsService.getStops().subscribe();
+    }
+    return stops.filter(stop =>
+      stop.south || stop.west
+    );
+  });
+
+  availableTimesForRoute = computed(() => {
+    const values = this.formValues();
+    const destName = values?.destination;
+    const selectedDate = values?.date; // Формат: "YYYY-MM-DD"
+    const trips = this.availableTrips();
+
+    if (!destName || !selectedDate || trips.length === 0) return [];
+
+    return trips.filter(t => {
+      const tripDateOnly = new Date(t.date).toISOString().split('T')[0];
+
+      const matchesDate = tripDateOnly === selectedDate;
+
+      const matchesRoute = t.route.endStop.stopId.name === destName ||
+        t.route.stops.some(s => s.stopId.name === destName);
+
+      const hasSeats = t.availableSeats > 0;
+
+      const isPast = this.isTimePast(t.route.startHour);
+
+      return matchesDate && matchesRoute && hasSeats && !isPast;
+    });
   });
 
   routeOrderValidator(): ValidatorFn {
@@ -147,6 +245,36 @@ export class Booking {
         ? null
         : { invalidOrder: true };
     };
+  }
+
+  getArrivalTimeForStop(trip: Trip): string {
+    const destName = this.formValues()?.destination;
+    if (!destName) return '';
+
+    if (trip.route.endStop.stopId.name === destName) {
+      return trip.route.arrivalHour;
+    }
+
+    const intermediateStop = trip.route.stops.find(s => s.stopId.name === destName);
+    return intermediateStop ? intermediateStop.arrivalTime : trip.route.arrivalHour;
+  }
+
+  isTimePast(tripHour: string): boolean {
+    const selectedDate = this.formValues()?.date;
+    const today = new Date().toISOString().split('T')[0];
+
+    if (selectedDate > today) return false;
+
+    const now = new Date();
+    const currentHour = now.getHours();
+    const currentMinutes = now.getMinutes();
+
+    const [tripH, tripM] = tripHour.split(':').map(Number);
+
+    if (tripH > currentHour) return false;
+    if (tripH === currentHour && tripM > currentMinutes) return false;
+
+    return true;
   }
 
   onSubmit(): void {
