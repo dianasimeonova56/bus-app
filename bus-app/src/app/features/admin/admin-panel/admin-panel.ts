2d142e3ef6429cbda4eb6dfd7fd95203949c4cc3
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-03-15 15:39:31 +0200

    FE: add edit route to admin panel

--- bus-app/src/app/features/admin/admin-panel/admin-panel.ts
+++ bus-app/src/app/features/admin/admin-panel/admin-panel.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AddTransportOperator } from "../add-transport-operator/add-transport-operator";
 import { AddStop } from "../../stops/add-stop/add-stop";
 import { AddRoute } from "../../routes/add-route/add-route";
+import { EditRoute } from "../edit-route/edit-route";
 
 @Component({
   selector: 'app-admin-panel',
-  imports: [AddTransportOperator, AddStop, AddRoute],
+  imports: [AddTransportOperator, AddStop, AddRoute, EditRoute],
   templateUrl: './admin-panel.html',
   styleUrl: './admin-panel.css',
 })
