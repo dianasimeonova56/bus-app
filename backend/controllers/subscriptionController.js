fda0e73f8380d344cc5539cc1446aca7ed6c6af4
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-03-15 20:41:20 +0200

    BE: subscription controller

---
+++ backend/controllers/subscriptionController.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import subscriptionService from '../services/subscriptionService.js';
+
+const subscriptionController = Router();
+
+subscriptionController.post('/buy-subscription', async (req, res) => {
+    try {
+        const subscriptionData = req.body; 
+        const userId = req.user?._id || subscriptionData.userId;
+
+        const session = await subscriptionService.createCheckoutSession(userId, subscriptionData);
+
+        res.status(200).json({ checkoutUrl: session.url });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({ error: err.message });
+    }
+});
+
+subscriptionController.post('/create', async (req, res) => {
+    try {
+        const subscriptionData = req.body.data;
+
+        const subscription = await subscriptionService.createSubscription(subscriptionData);
+
+        res.status(200).json({ subscription: subscription });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({ error: err.message });
+    }
+});
+
+export default subscriptionController;
