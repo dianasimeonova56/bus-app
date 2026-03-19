bc71836305a6b037591e7e9f8794c4e355ddb325
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-03-19 23:26:43 +0200

    FE: add component to admin panel

--- bus-app/src/app/features/admin/admin-panel/admin-panel.ts
+++ bus-app/src/app/features/admin/admin-panel/admin-panel.ts
@@ -4,13 +4,14 @@ import { AddStop } from "../../stops/add-stop/add-stop";
 import { AddRoute } from "../../routes/add-route/add-route";
 import { EditRoute } from "../edit-route/edit-route";
 import { AddSubscription } from "../add-subscription/add-subscription";
+import { Verifications } from "../verifications/verifications";
 
 @Component({
   selector: 'app-admin-panel',
-  imports: [AddTransportOperator, AddStop, AddRoute, EditRoute, AddSubscription],
+  imports: [AddTransportOperator, AddStop, AddRoute, EditRoute, AddSubscription, Verifications],
   templateUrl: './admin-panel.html',
   styleUrl: './admin-panel.css',
 })
 export class AdminPanel {
-
+  
 }
