8189b0150bb9841110267795ae7d03be1ea7232a
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-03-22 22:17:28 +0200

    BE; add  pagination on news, add auth middleware for delete

--- backend/controllers/newsController.js
+++ backend/controllers/newsController.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import newsService from '../services/newsService.js'
+import { auth, isAuth } from '../middlewares/authMiddleware.js';
+import { getPagination, formatResponse } from '../utils/paginationUtil.js';
 
 const newsController = express.Router();
 
-newsController.post('/create', async (req, res) => {
+newsController.post('/create', auth, isAuth, async (req, res) => {
     try {
         const { newsData } = req.body;
 
         if (!newsData) {
             return res.status(400).json({ message: 'No newsData provided' });
         }
+        const adminId = req.user.id;
 
-        const news = await newsService.create(newsData);
+        if (!adminId) {
+            return res.status(401).json({ message: 'You must be logged in to create news' });
+        }
+
+        const news = await newsService.create(newsData, adminId);
 
         res.status(201).json(news);
     } catch (err) {
@@ -22,14 +29,18 @@ newsController.post('/create', async (req, res) => {
 
 newsController.get('/', async (req, res) => {
     try {
-        const news = await newsService.getAll();
+        const { page, limit, skip } = getPagination(req.query);
 
-        res.status(200).json(news);
+        const { news, totalDocs } = await newsService.getAll(skip, limit);
+
+        const response = formatResponse(news, totalDocs, page, limit);
+
+        res.status(200).json(response);
     } catch (err) {
         console.error('Error while fetching news:', err.message);
         res.status(400).json({ message: err.message });
     }
-})
+});
 
 newsController.get('/:id', async (req, res) => {
     try {
@@ -43,12 +54,12 @@ newsController.get('/:id', async (req, res) => {
     }
 })
 
-newsController.post('/:id/delete', async (req, res) => {
+newsController.post('/:id/delete', auth, isAuth, async (req, res) => {
     try {
         const id = req.params.id;
         await newsService.delete(id);
 
-        res.status(201).json({messgae: "Deleted news!"});
+        res.status(201).json({ messgae: "Deleted news!" });
     } catch (err) {
         console.error('Error while deleting news:', err.message);
         res.status(400).json({ message: err.message });
