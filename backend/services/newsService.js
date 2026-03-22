6add9ef2b9dcc59ba442d5802c7c42fdd4af10ec
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-03-22 22:16:17 +0200

    BE: add pagination for news

--- backend/services/newsService.js
+++ backend/services/newsService.js
@@ -1,11 +1,17 @@
 import News from '../models/News.js';
 
 export default {
-    async getAll(limit = 10) {
-        return await News.find()
-            .sort({ isImportant: -1, createdAt: -1 })
-            .limit(limit)
-            .populate('author', 'firstName lastName');
+    async getAll(skip, limit) {
+        const [news, totalDocs] = await Promise.all([
+            News.find()
+                .sort({ isImportant: -1, createdAt: -1 })
+                .skip(skip)
+                .limit(limit)
+                .populate('author', 'firstName lastName'),
+            News.countDocuments()
+        ]);
+
+        return { news, totalDocs };
     },
 
     async getById(id) {
