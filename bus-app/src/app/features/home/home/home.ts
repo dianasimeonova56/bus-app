8e68d13cb3ebbf4bebbc24add4c3838ef6a4062b
dianasimeonova56 <diana.simeonova56@gmail.com>
2026-05-23 11:20:50 +0300

    FE: home page updates - add in news section

--- bus-app/src/app/features/home/home/home.ts
+++ bus-app/src/app/features/home/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from "../../hero/hero";
 import { RoutesTabs } from "../../routes/routes-tabs/routes-tabs";
+import { NewsSection } from "../../news/news-section/news-section";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, RoutesTabs],
+  imports: [Hero, RoutesTabs, NewsSection],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
